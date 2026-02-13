346fc129a677d1bb1f86779525cc5d41df7108e8
Аршинов Алексей <xstonumx@yandex.ru>
2026-02-13 15:49:59 +0300

    feat(mlang): format functon variable

--- server/mlang_formatter/tests/new_rules.rs
+++ server/mlang_formatter/tests/new_rules.rs
@@ -1,2 +1,13 @@
 #[macro_use]
 mod helper;
+
+#[test]
+fn format_function_variable() {
+    assert_fmt!(
+        r#"#
+перем проверитьВложение = Функция(_вложение) {
+   Вернуть true;
+};
+"#
+    );
+}
